89585cb46c0d5c972e3398364d5b4a6c1dd13414
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-08-16 20:30:41 +0200

    Create the budget table migration

--- Budget.SQL/2_create_categories_table.sql
+++ Budget.SQL/2_create_categories_table.sql
@@ -9,5 +9,5 @@ CREATE TABLE categories (
 	category_name VARCHAR(255) NOT NULL,
 	category_created_at DATETIME2 NOT NULL CONSTRAINT df_category_created_at DEFAULT SYSDATETIME(),
 	category_updated_at DATETIME2 NOT NULL,
-	CONSTRAINT pk_category_id PRIMARY KEY (category_id),
+	CONSTRAINT pk_category_id PRIMARY KEY (category_id)
 );
