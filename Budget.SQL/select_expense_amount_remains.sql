ad85cdc3218cdc02cbba8d6d04d0244e27b33cbe
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-08-17 19:57:32 +0200

    Fix the script to determin remain expense amount

--- Budget.SQL/select_expense_amount_remains.sql
+++ Budget.SQL/select_expense_amount_remains.sql
@@ -2,16 +2,20 @@ USE budget;
 GO
 
 SELECT
+	c.category_id,
+	c.category_name,
 	b.budget_month,
 	b.budget_year,
 	cb.category_budget_amount,
-	e.expense_amount,
 	SUM(e.expense_amount) AS expense_amount_sum,
 	cb.category_budget_amount - SUM(e.expense_amount) AS expense_amount_remains
 FROM category_budgets AS cb
-INNER JOIN budgets AS b
+LEFT JOIN budgets AS b
 ON cb.budget_id = b.budget_id
 LEFT JOIN expenses AS e
 ON cb.category_id = e.category_id
+LEFT JOIN categories AS c
+ON c.category_id = cb.category_id
 WHERE b.budget_month = 8 AND b.budget_year = 2026
-GROUP BY b.budget_month, b.budget_year, cb.category_budget_amount, e.expense_amount;
+GROUP BY c.category_id, c.category_name, b.budget_month, b.budget_year, cb.category_budget_amount
+ORDER BY c.category_id ASC;
