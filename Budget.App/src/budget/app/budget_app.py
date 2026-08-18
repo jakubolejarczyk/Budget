09cfb7594b10e85d053ecc31c7d685ba53116e5d
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-08-18 23:50:04 +0200

    Create infinite loop until the is_running store value is true and store the last entered command

--- Budget.App/src/budget/app/budget_app.py
+++ Budget.App/src/budget/app/budget_app.py
@@ -1,3 +1,13 @@
+from budget.store.budget_store import BudgetStore
+
 class BudgetApp:
+    def __init__(self):
+        self.budget_store = BudgetStore()
+
     def run(self):
-        print("Budget App works!")
+        while self.budget_store.is_running:
+            self.budget_store.command = input("Enter command: ")
+            if self.budget_store.command == "exit":
+                self.budget_store.is_running = False
+            else:
+                print(f"The entered command is: {self.budget_store.command}")
---
+++ Budget.App/src/budget/store/budget_store.py
@@ -0,0 +1,4 @@
+class BudgetStore:
+    def __init__(self):
+        self.is_running = True
+        self.command = None
