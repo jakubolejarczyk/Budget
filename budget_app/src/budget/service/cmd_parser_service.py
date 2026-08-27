7b4324687dcb2dcc4a530767550b8841772e0f2b
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-08-27 12:51:00 +0200

    Fetch the program from the provided command

--- budget_app/src/budget/service/cmd_parser_service.py
+++ budget_app/src/budget/service/cmd_parser_service.py
@@ -3,9 +3,18 @@ from budget.model import CmdParserModel
 
 class CmdParserService:
     def parse(self, cmd: str) -> CmdParserModel:
+        cmd_items: list[str] = cmd.split(" ")
         return CmdParserModel(
-            program="",
+            program=self._get_program(cmd_items),
             program_args=[],
             command="",
             command_args=[]
         )
+
+    def _get_program(self, cmd_items: list[str]) -> str | None:
+        if len(cmd_items) > 0:
+            program: str = cmd_items[0]
+            if program == "":
+                return None
+            return program
+        return None
--- budget_app/test/test_budget/service/test_cmd_parser_service.py
+++ budget_app/test/test_budget/service/test_cmd_parser_service.py
@@ -3,11 +3,22 @@ from budget.service import CmdParserService
 
 
 class TestCmdParserService:
-    def test_should_parse_empty_cmd(self):
+    def test_should_parse_empty_cmd(self) -> None:
         cmd_parser_service = CmdParserService()
         result = cmd_parser_service.parse("")
         expect = CmdParserModel(
-            program="",
+            program=None,
+            program_args=[],
+            command="",
+            command_args=[]
+        )
+        assert result == expect
+
+    def test_should_parse_when_only_program_is_provided(self) -> None:
+        cmd_parser_service = CmdParserService()
+        result = cmd_parser_service.parse("aaa")
+        expect = CmdParserModel(
+            program="aaa",
             program_args=[],
             command="",
             command_args=[]
