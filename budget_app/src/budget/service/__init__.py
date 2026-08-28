b797b311573a197024c6d63c94ff504cb2dc6a39
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-08-28 11:16:42 +0200

    Create the logic and test to get input service

--- budget_app/src/budget/service/__init__.py
+++ budget_app/src/budget/service/__init__.py
@@ -1,2 +1,3 @@
 from .cmd_parser_service import *
+from .get_input_service import *
 from .program_service import *
---
+++ budget_app/src/budget/service/get_input_service.py
@@ -0,0 +1,6 @@
+class GetInputService:
+    def get_input(self):
+        return self._get_input_from_keyboard()
+
+    def _get_input_from_keyboard():
+        return input("> ")
