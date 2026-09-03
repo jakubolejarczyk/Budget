03e15ff21e50ae3d25725a428e0356ed393e31e4
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-09-03 13:00:50 +0200

    Create basic connection to the database

--- budget_app/src/budget/main.py
+++ budget_app/src/budget/main.py
@@ -1,7 +1,21 @@
+from os import getenv
+from dotenv import load_dotenv
+from mssql_python import connect
 from budget.app import BudgetApp
 
 
 def main() -> None:
+    load_dotenv()
+    connection_string = getenv("SQL_CONNECTION_STRING")
+    if connection_string is None:
+        raise ValueError(
+            "SQL_CONNECTION_STRING environment variable is not set.")
+    connection = connect(connection_string)
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM expenses")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
     BudgetApp().run()
 
 
