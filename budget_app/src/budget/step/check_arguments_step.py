43d229bc3d67de3e9e7a5da9191fccb5647dd3fb
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-09-10 20:42:29 +0200

    Check if value is single or multiple value

--- budget_app/src/budget/step/check_arguments_step.py
+++ budget_app/src/budget/step/check_arguments_step.py
@@ -13,6 +13,9 @@ class CheckArgumentsStep:
 
     def _check_argument(self, config: ProgramConfigModel | CommandConfigModel, config_argument: ArgumentConfigModel, arguments: list[ArgumentModel]) -> None:
         self._check_is_required_validator(config, config_argument, arguments)
+        self._check_can_have_value(config, config_argument, arguments)
+        self._check_can_have_multiple_values(
+            config, config_argument, arguments)
 
     def _check_is_required_validator(self, config: ProgramConfigModel | CommandConfigModel, config_argument: ArgumentConfigModel, arguments: list[ArgumentModel]) -> None:
         if config_argument.is_required == False:
@@ -24,4 +27,42 @@ class CheckArgumentsStep:
                 return
         Store.are_arguments_correct = False
         error = f'Parameter "{config_argument.name}" is required for entity "{config.name}" but was not provided.'
+        print(error)
+
+    def _check_can_have_value(self, config: ProgramConfigModel | CommandConfigModel, config_argument: ArgumentConfigModel, arguments: list[ArgumentModel]) -> None:
+        if config_argument.can_have_value == True:
+            return
+        current_argument: ArgumentModel = None
+        for argument in arguments:
+            if argument.name == config_argument.name and argument.type == "argument":
+                current_argument = argument
+                break
+            if argument.name == config_argument.alias and argument.type == "alias":
+                current_argument = argument
+                break
+        if current_argument is None:
+            return
+        if current_argument.value == None:
+            return
+        Store.are_arguments_correct = False
+        error = f'Parameter "{config_argument.name}" has value for entity "{config.name}" but is not permitted.'
+        print(error)
+
+    def _check_can_have_multiple_values(self, config: ProgramConfigModel | CommandConfigModel, config_argument: ArgumentConfigModel, arguments: list[ArgumentModel]) -> None:
+        if config_argument.can_have_multiple_values == True:
+            return
+        current_argument: ArgumentModel = None
+        for argument in arguments:
+            if argument.name == config_argument.name and argument.type == "argument":
+                current_argument = argument
+                break
+            if argument.name == config_argument.alias and argument.type == "alias":
+                current_argument = argument
+                break
+        if current_argument is None:
+            return
+        if type(current_argument.value) is not list:
+            return
+        Store.are_arguments_correct = False
+        error = f'Parameter "{config_argument.name}" has multiple values for entity "{config.name}" but is not permitted.'
         print(error)
